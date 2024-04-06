4c322773e75eb5bdcb612786fdac1525b68d9ccc
kaiqueBellmont <kaiquebellmont@gmail.com>
2024-04-06 13:17:38 -0300

    fundamentals

---
+++ fundamentals/02_var_1.dart
@@ -0,0 +1,12 @@
+main() {
+  // This is a variable declaration
+  // The type of the variable is inferred from the value assigned to it
+  var message = 'hello world!';
+  print(message);
+
+  int a = 10;
+  double pi = 3.14;
+  bool isTrue = true;
+  String name = 'Dart';
+  print(a * pi);
+}
